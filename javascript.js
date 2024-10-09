e98983f1f08540cfe757af1374fcab938d139add
XinnyDonut <xinxin.wang0703@gmail.com>
2024-10-10 08:29:45 +1300

    Add grid dynamicly

--- javascript.js
+++ javascript.js
@@ -1,17 +1,29 @@
+const btn=document.querySelector("button");
+const container=document.querySelector(".container");
+container.style.width="500px";
+containerWidth=container.offsetWidth;
+console.log(containerWidth)
 
 
-function createGrid(){
-    const container=document.querySelector(".container");
-    for(let i=0;i<16;i++){
-        const row=document.createElement("row");
-        for(let j=0;j<16;j++){
+function createNewGrid(num){
+    
+    console.log();
+    for(let i=0;i<num;i++){
+        const col=document.createElement("col");
+        for(let j=0;j<num;j++){
             const square=document.createElement("div");
             square.classList.add("square");
-            //square.style.color="blue";
-            row.appendChild(square); 
+            square.style.width=(containerWidth/num)+"px";
+            square.style.borderStyle="dotted";
+            square.style.height=(containerWidth/num)+"px";
+            square.style.backgroundColor="white";
+            square.addEventListener('mouseenter',()=>{
+                square.style.backgroundColor="pink";
+            })
+            col.appendChild(square); 
         }
-        container.appendChild(row);   
+        container.appendChild(col);   
     }
 }
 
-createGrid();
+createNewGrid(15);
