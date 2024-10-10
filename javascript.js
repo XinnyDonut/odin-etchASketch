779c59fa2144367d171dde74a0c8c2b71271239c
XinnyDonut <xinxin.wang0703@gmail.com>
2024-10-10 16:30:13 +1300

    Add button to resetcanvas

--- javascript.js
+++ javascript.js
@@ -2,19 +2,30 @@ const btn=document.querySelector("button");
 const container=document.querySelector(".container");
 container.style.width="500px";
 containerWidth=container.offsetWidth;
-console.log(containerWidth)
 
 
-function createNewGrid(num){
-    
-    console.log();
+btn.addEventListener('click',()=>{
+    let input=prompt("how many grid you would like per side, 40 is maximum");
+    let num=parseInt(input);
+    if (num>40||num<0||isNaN(num)){
+        alert("enter a valid number!");
+    }else{
+        while(container.firstChild){
+            container.removeChild(container.firstChild);
+        }
+        createNewGrid(num);
+    }
+})
+
+
+function createNewGrid(num){   
     for(let i=0;i<num;i++){
         const col=document.createElement("col");
         for(let j=0;j<num;j++){
             const square=document.createElement("div");
             square.classList.add("square");
             square.style.width=(containerWidth/num)+"px";
-            square.style.borderStyle="dotted";
+            square.style.borderStyle="solid";
             square.style.height=(containerWidth/num)+"px";
             square.style.backgroundColor="white";
             square.addEventListener('mouseenter',()=>{
